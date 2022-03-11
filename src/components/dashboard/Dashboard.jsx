138e9a143e8b5cae610e091ac9fa6f334d60395d
Michael Chilaka <obichilaka@gmail.com>
2022-03-11 09:26:09 +0100

    mapping the elements

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,11 +2,34 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: "50px",
     padding: theme.spacing(3, 2),
+  },
+  flex: {
+    display: "flex",
+    alignItems: "center",
+  },
+  topicsWindow: {
+    width: "30%",
+    height: "300px",
+    borderRight: "1px solid grey",
+  },
+  chatWindow: {
+    width: "70%",
+    height: "300px",
+  },
+  chatBox: {
+    width: "85%",
+  },
+  button: {
+    width: "15%",
   },
 }));
 
@@ -15,10 +38,32 @@ const Dashboard = () => {
   return (
     <div>
       <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          Blabla
+        <Typography variant="h4" component="h4">
+          Chat App
         </Typography>
-        <Typography component="p">Michael</Typography>
+        <Typography variant="h5" component="h5">
+          Topic placeholder
+        </Typography>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {["topic"].map((topic) => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, i) => (
+              <div className={classes.flex} key={i}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="p">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={classes.flex}></div>
       </Paper>
     </div>
   );
